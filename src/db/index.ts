fb30033d5c7629b944e02c03421a704be9b7dd75
Ulises <ulisesapaza.280@gmail.com>
2025-03-26 00:34:03 -0300

    refactor: change from node-postgres to postgres.js

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 const db = drizzle(process.env.DATABASE_URL!);
 export default db;
