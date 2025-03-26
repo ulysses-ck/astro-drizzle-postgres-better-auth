60198a09295c1ca7a7434fe9c9313b600e465bde
Ulises <ulisesapaza.280@gmail.com>
2025-03-26 00:53:19 -0300

    fix: react issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,9 +18,11 @@ export default defineConfig({
   ),
   vite: {
     resolve: {
-      alias: import.meta.env.PROD ? {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
         "react-dom/server": "react-dom/server.edge",
-      } : undefined
-    }
-  }
+      },
+    },
+  },
 });
