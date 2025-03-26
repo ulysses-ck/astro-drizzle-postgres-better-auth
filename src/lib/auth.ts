36d1fb0371df974d062d3cb37b12111224e97273
Ulises <ulisesapaza.280@gmail.com>
2025-03-25 21:47:45 -0300

    fix: include schemas

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,20 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db"; 
+import { user, account, session, verification } from "@/db/auth-schema";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "pg"
+        provider: "pg",
+        schema: {
+            user,
+            account,
+            session,
+            verification
+        },
     }),
     emailAndPassword: {
         enabled: true
-    }
+    },
+    trustedOrigins: ["http://localhost:4321"]
 })
