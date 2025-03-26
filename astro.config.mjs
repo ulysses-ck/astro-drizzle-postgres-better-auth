498bffef0409481393dc4c0e2a7cb283d86c4f4b
Ulises <ulisesapaza.280@gmail.com>
2025-03-25 22:00:36 -0300

    chore: react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server"
+  output: "server",
+  integrations: [react()]
 });
